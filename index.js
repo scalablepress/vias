de2d968bdb8396a507596fe6322f54efbb4750fb
Albert <sero.albert@gmail.com>
2016-06-09 14:57:43 -0700

    Update constants

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ export {default} from './src/Vias';
 
 export {default as ViasPromise} from './src/ViasPromise';
 export {default as Model} from './src/Model';
-export {default as ViasDepend} from './src/ViasDepend';
+export {default as Depend} from './src/Depend';
 export {viasPromiseValue, viasPromiseState} from './src/util';
-export {MODEL_UPDATED} from './src/constants';
+export {
+  MODEL_UPDATED,
+  SECOND,
+  MINUTE,
+  HOUR,
+  FOREVER,
+  DOCUMENT_SHAPE,
+  OBJECT_SHAPE,
+  ARRAY_SHAPE,
+} from './src/constants';
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,18 @@
 export const MODEL_CACHE_UPDATED = 'VIAS_MODEL_CACHE_UPDATED';
-export const MODEL_PROMISE_UPDATED = 'VIAS_MODEL_PROMISE_UPDATED';
+export const SECOND = 1000;
+export const MINUTE = 60 * SECOND;
+export const HOUR = 60 * MINUTE;
+export const FOREVER = -1;
+
+export const DOCUMENT_SHAPE = () => null;
+export const OBJECT_SHAPE = function (result) {
+  let paths = [];
+  for (let key in result) {
+    if (result.hasOwnProperty(key)) {
+      paths.push(key);
+    }
+  }
+  return paths;
+};
+
+export const ARRAY_SHAPE = OBJECT_SHAPE;
