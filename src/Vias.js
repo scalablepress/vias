4efa5b9972796e873037b36601d3bf8252827e01
Albert <sero.albert@gmail.com>
2016-06-14 16:36:25 -0700

    Add fulfill to static method

--- src/Vias.js
+++ src/Vias.js
@@ -1,22 +1,108 @@
 import _ from 'lodash';
 import async from 'async';
 import React from 'react';
+import hoistNonReactStatic from 'hoist-non-react-statics';
+
 
 import ViasPromise from './ViasPromise';
 import {ViasDependPromise} from './Depend';
 
-function vias(options = {}) {
+function fulfillReady(promises, onPromiseFinish, cb) {
+  async.each(promises, function (promise, eCb) {
+    promise.exec((err, result) => {
+      if (onPromiseFinish) {
+        onPromiseFinish(err, promise, result);
+      }
+      return eCb(err, result);
+    });
+  }, cb);
+}
+
+function extendPromises(props, promiseMap) {
+  let promises = {};
+  for (let key in props) {
+    let promise = props[key];
+    if (promise instanceof ViasDependPromise) {
+      let dependencies = {};
+      for (let key in promise.dependencies) {
+        let dependency = promise.dependencies[key];
+        if (dependency.id && promiseMap[dependency.id]) {
+          dependencies[key] = promiseMap[dependency.id];
+        }
+      }
+      promise.update(_.extend({}, promise.dependencies, dependencies));
+    }
+
+    if (promise instanceof ViasPromise && promise.id && promiseMap[promise.id]) {
+      promises[key] = promiseMap[promise.id];
+    }
+  }
+  return _.extend({}, props, promises);
+}
+
+function fulfilling(props, promiseMap) {
+  let toFulfill = [];
+  let promiseProps = extendPromises(props, promiseMap);
+  for (let key in promiseProps) {
+    if (promiseProps[key] instanceof ViasPromise) {
+      let promise = promiseProps[key];
+      if (promise.id && promiseMap[promise.id]) {
+        promise = promiseMap[promise.id];
+      } else if (promise.id) {
+        promiseMap[promise.id] = promise;
+      }
+
+      if (!promise.started) {
+        toFulfill.push(promise);
+      }
+    }
+  }
+  return toFulfill;
+}
+
+function allFulfilled(promiseMap) {
+  return _.every(promiseMap, (promise) => {
+    return promise.rejected || promise.fulfilled;
+  });
+}
+
+function fulfillAll(props, cb) {
+  let promiseMap = {};
+  let calledBack = false;
+
+  function callback(err, result) {
+    if (!calledBack) {
+      return cb(err, result);
+    }
+    calledBack = true;
+  }
+
+  function iterate() {
+    let toFulfill = fulfilling(props, promiseMap);
+    if (toFulfill.length <= 0) {
+      if (allFulfilled(promiseMap)) {
+        return callback();
+      }
+      return;
+    }
+
+    fulfillReady(toFulfill, (err) => {
+      if (err) {
+        return callback(err);
+      }
+      return iterate();
+    });
+  }
+
+  iterate();
+}
+
+function vias() {
   return function (WrappedComponent) {
     class Vias extends React.Component {
-      static fulfillPromises(promises, onPromiseFinish, cb) {
-        async.each(promises, function (promise, eCb) {
-          promise.exec((err, result) => {
-            if (onPromiseFinish) {
-              onPromiseFinish(err, promise, result);
-            }
-            return eCb(err, result);
-          });
-        }, cb);
+      // Fulfill ViasPromise and ready ViasDependPromise
+      static fulfillAll(props, cb) {
+        fulfillAll(props, cb);
       }
 
       constructor(props) {
@@ -36,47 +122,18 @@ function vias(options = {}) {
         }
       }
 
-      extendPromises(promiseMap) {
-        let promises = {};
-        for (let key in promiseMap) {
-          let promise = promiseMap[key];
-          if (promise instanceof ViasDependPromise) {
-            let dependencies = {};
-            for (let key in promise.dependencies) {
-              let dependency = promise.dependencies[key];
-              if (dependency.id && this.promises[dependency.id]) {
-                dependencies[key] = this.promises[dependency.id];
-              }
-            }
-            promise.update(_.extend({}, promise.dependencies, dependencies));
-          }
-
-          if (promise instanceof ViasPromise && promise.id && this.promises[promise.id]) {
-            promises[key] = this.promises[promise.id];
-          }
-        }
-        return _.extend({}, promiseMap, promises);
+      extendPromises(props) {
+        return extendPromises(props, this.promises);
       }
 
-      fulfillPromises(props) {
-        let toFulfill = [];
-        let promisesMap = this.extendPromises(props);
-        for (let key in promisesMap) {
-          if (promisesMap[key] instanceof ViasPromise) {
-            let promise = promisesMap[key];
-            if (promise.id && this.promises[promise.id]) {
-              promise = this.promises[promise.id];
-            } else if (promise.id) {
-              this.promises[promise.id] = promise;
-            }
 
-            if (!promise.started) {
-              toFulfill.push(promise);
-            }
-          }
+      fulfillPromises(props) {
+        let toFulfill = fulfilling(props, this.promises);
+        if (toFulfill.length <= 0) {
+          return;
         }
 
-        Vias.fulfillPromises(toFulfill, (err, promise) => {
+        fulfillReady(toFulfill, (err, promise) => {
           this.promises[promise] = Object.assign({}, promise);
           this.fulfillPromises(this.props);
           this.subscribeModel(promise.cacheModel());
@@ -105,7 +162,7 @@ function vias(options = {}) {
         }
       }
     }
-    return Vias;
+    return hoistNonReactStatic(Vias, WrappedComponent);
   };
 }
 
