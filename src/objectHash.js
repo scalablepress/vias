ce5f34329795a9b5c05bdee7c2425f3b34b51bad
Albert <sero.albert@gmail.com>
2016-06-29 12:15:26 -0700

    Fix hash of nested object

--- src/objectHash.js
+++ src/objectHash.js
@@ -1,8 +1,14 @@
 import _ from 'lodash';
 
-export default function (data) {
+export default function objectHash(data) {
   if (_.isPlainObject(data)) {
-    return JSON.stringify(_.sortBy(_.pairs(data), ([key]) => key));
+    return JSON.stringify(_.sortBy(_.pairs(_.mapValues(data, objectHash)), ([key]) => key));
+  }
+  if (_.isArray(data)) {
+    return JSON.stringify(_.map(data, objectHash));
+  }
+  if (_.isString(data)) {
+    return data;
   }
   return JSON.stringify(data);
 }
